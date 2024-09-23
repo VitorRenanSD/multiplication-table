720e8366241eae9382d851a16afdddd0ba82678b
Vitor Renan <167242612+VitorRenanSD@users.noreply.github.com>
2024-09-23 08:13:38 -0300

    Update multiplication_table_ptbr.py

--- multiplication_table_ptbr.py
+++ multiplication_table_ptbr.py
@@ -1,4 +1,6 @@
-numero = int(input('Criar uma tabela de multiplicação pro número: '))
+numero = int(input('Criar uma tabela pro número: '))
 
+print('-' * 15)
 for i in range(1, 11):
-    print(f'{number} * {i:2} = {number * i}')
+    print(f'\033[1;33m {number} * {i:2} = {number * i:2} \033[m')
+print('-' * 15)
