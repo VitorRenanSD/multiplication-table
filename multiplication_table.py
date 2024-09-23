3f4294a1bbbfab5cf2d4f89c02444eeac4920d87
Vitor Renan <167242612+VitorRenanSD@users.noreply.github.com>
2024-09-23 08:02:00 -0300

    Update multiplication_table.py
    Added a bit of beauty in the output of the code, using colors in Python

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,4 +1,6 @@
 number = int(input('Create a multiplication table to number: '))
 
+print('-' * 15)
 for i in range(1, 11):
-    print(f'{number} * {i:2} = {number * i}')
+    print(f'\033[1;33m {number} * {i:2} = {number * i:2} \033[m')
+print('-' * 15)
