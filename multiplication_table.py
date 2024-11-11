8016c14d3c0b7871b45f34083e3032bfd6adb034
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2024-11-11 10:56:47 -0300

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,6 +1,4 @@
 number = int(input('Create a multiplication table to number: '))
 
-print('-' * 15)
 for i in range(1, 11):
     print(f'\033[1;33m {number} * {i:2} = {number * i:2} \033[m')
-print('-' * 15)
